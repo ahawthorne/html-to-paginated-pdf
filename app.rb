ebb4d2cf4821802c3c6ea4546825d9ae9989f62f
Andrew Hawthorne <andrew@domain7.com>
2012-01-10 09:33:18 -0800

    Modified to use wkhtmltopdf
    Removed requirements for OSX, wkpdf, ruby cocoa

--- app.rb
+++ app.rb
@@ -12,13 +12,13 @@ end
 get '/html_to_paginated_pdf.pdf' do
   @uri = params[:uri]
   filename = "paginated_pdf-#{Time.now.to_i}.pdf"
-  cmd = "wkpdf --source #{@uri} --output #{filename} --margin 1 --print-background no --paginate yes --hcenter"
+  cmd = "wkhtmltopdf-amd64 -s letter -B 0 -T 0 -L 0 -R 0 --print-media-type #{@uri} #{filename}"
   `#{cmd}`
   file = File.open(filename, "rb")
   contents = file.read
   File.delete(filename)
   file.close
-  
+
   content_type 'application/pdf'
   contents
 end
--- config.ru
+++ config.ru
@@ -1,9 +1,14 @@
+require 'rubygems'
+require 'bundler'
+
+Bundler.require
+
 require 'sinatra'
- 
+
 Sinatra::Application.default_options.merge!(
   :run => false,
   :env => :production
 )
- 
+
 require 'app'
 run Sinatra.application
